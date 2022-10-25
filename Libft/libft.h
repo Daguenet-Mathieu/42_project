50bf6fffc8246595c4e87f8ac4d900d36c71f70f
Mathieu <mathieu.daguenet@gmail.com>
2022-10-25 02:00:17 +0200

    maj

--- Libft/libft.h
+++ Libft/libft.h
@@ -1,8 +1,16 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef LIBFT_H
+#define LIBFT_H
+
 #include<unistd.h>
 #include<stdlib.h>
 #include<stddef.h>
+
+typedef struct	s_list
+{
+	void	*content;
+	struct	s_list *next;
+}		t_list;
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
@@ -26,4 +34,15 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
